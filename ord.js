440b8fb1833a5586a61b617f7e3de5cb6dfac69d
HelenNilson <77539899+HelenNilson@users.noreply.github.com>
2021-03-02 01:21:55 +0200

    Add files via upload

---
+++ ord.js
@@ -0,0 +1,97 @@
+PROPOSAL = [{
+    group: 'Вареники',
+    cash: 'грн',
+    children: [{
+            name: '- з картоплею та шкварочками',
+            price: '65',
+            count: 0
+        },
+        {
+            name: '- з картоплею та грибами',
+            price: '70',
+            count: 0
+        },
+        {
+            name: '- з капустою',
+            price: '65',
+            count: 0
+        },
+        {
+            name: '- з сиром кисломоочним',
+            price: '80',
+            count: 0
+        },
+        {
+            name: '- з лівером',
+            price: '120',
+            count: 0
+        },
+        {
+            name: '- з сулугуні та помідорами',
+            price: '140',
+            count: 0
+        },
+        {
+            name: '- з м\'ясом (свинина)',
+            price: '135',
+            count: 0
+        }
+    ],
+
+    group: 'Зрази',
+    cash: 'грн',
+    children: [{
+            name: '- з м\'ясом',
+            price: '110',
+            count: 0
+        },
+        {
+            name: '- з грибами',
+            price: '85',
+            count: 0
+        },
+        {
+            name: '- з капустою',
+            price: '65',
+            count: 0
+        }
+    ],
+    group: 'Інші смаколики',
+    cash: 'грн',
+    children: [{
+            name: 'Пельмені',
+            price: '120',
+            count: 0
+        },
+        {
+            name: 'Манти',
+            price: '160',
+            count: 0
+        },
+        {
+            name: 'Хінкалі',
+            price: '150',
+            count: 0
+        },
+        {
+            name: 'Перці фаршировані',
+            price: '120',
+            count: 0
+        },
+        {
+            name: 'Голубці',
+            price: '85',
+            count: 0
+        },
+        {
+            name: 'Котлети рибні',
+            price: '140',
+            count: 0
+        },
+        {
+            name: 'Паштет печінковий (250гр)',
+            price: '60',
+            count: 0
+        }
+    ],
+}];
